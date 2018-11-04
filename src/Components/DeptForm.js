6ff65e32de1eef1a64eca9f6c420107b91dbd34c
jarretbryan <jarret.bryan@gmail.com>
2018-11-04 14:16:21 -0500

    cleanup

--- src/Components/DeptForm.js
+++ src/Components/DeptForm.js
@@ -1,40 +1,6 @@
 import React, { Component } from 'react';
 import DeptAdapter from '../api/DeptAdapter'
-
 import styled, { css } from 'styled-components'
-
-
-//styling
-
-const Form = styled.form.attrs({
-    className: "pa4 black-80"
-})``
-
-const Measure = styled.div.attrs({
-    className: "measure"
-})``
-
-const Label = styled.label.attrs({
-    className: "f6 b db mb2"
-})``
-
-const Input = styled.input.attrs({
-    className: "input-reset ba b--black-20 pa2 mb2 db w-100",
-    type: "text"
-})``
-
-const Button = styled.p.attrs({
-    className: "f6 link dim br-pill ph3 pv2 mb2 dib white"
-})`
-    background: blue
-    margin: 1em;
-
-    ${props => props.primary && css`
-    background: red;
-    color: white;
-  `}
-`
-
 class EmployeeForm extends Component {
 
     state = {
@@ -69,5 +35,36 @@ class EmployeeForm extends Component {
         );
     }
 }
+
+//styling
+
+const Form = styled.form.attrs({
+    className: "pa4 black-80"
+})``
+
+const Measure = styled.div.attrs({
+    className: "measure"
+})``
+
+const Label = styled.label.attrs({
+    className: "f6 b db mb2"
+})``
+
+const Input = styled.input.attrs({
+    className: "input-reset ba b--black-20 pa2 mb2 db w-100",
+    type: "text"
+})``
+
+const Button = styled.p.attrs({
+    className: "f6 link dim br-pill ph3 pv2 mb2 dib white"
+})`
+    background: blue
+    margin: 1em;
+
+    ${props => props.primary && css`
+    background: red;
+    color: white;
+  `}
+`
 
 export default EmployeeForm;
