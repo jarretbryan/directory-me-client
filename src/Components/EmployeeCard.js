046cbbc14c740de50e6f2307493af0a9042f7f77
jarretbryan <jarret.bryan@gmail.com>
2018-11-04 11:50:04 -0500

    Edit styling of employeecard

--- src/Components/EmployeeCard.js
+++ src/Components/EmployeeCard.js
@@ -3,19 +3,19 @@ import styled from 'styled-components'
 
 
 const Article = styled.article.attrs({
-    className: "center mw5 mw6-ns hidden mv4"
+    className: "mw5 center bg-white br3 pa3 pa4-ns mv3"
 })``
 
 const Title = styled.h1.attrs({
-    className: "f4 bg-near-black white mv0 pv2 ph3"
+    className: "f3 mb2"
 })``
 
 const Box = styled.div.attrs({
-    className: "pa3 bt"
+    className: "tl"
 })``
 
-const Text = styled.li.attrs({
-    className: "f6 f5-ns lh-copy measure mv0"
+const Text = styled.h2.attrs({
+    className: "f5 fw4 gray mt0"
 })``
 
 
@@ -23,12 +23,10 @@ const EmployeeCard = (props) => (
     <Article>
         <Title>{props.info.name}</Title>
         <Box>
-            <ul>
-                <Text>Department: {props.info.department.name} </Text>
-                <Text>Title: {props.info.title}</Text>
-                <Text>Office: {props.info.office}</Text>
-                <Text>Direct Line: {props.info.direct}</Text>
-            </ul>
+            <Text><strong>Department: </strong>{props.info.department.name} </Text>
+            <Text><strong>Title:</strong>{props.info.title}</Text>
+            <Text><strong>Office:</strong>{props.info.office}</Text>
+            <Text><strong>Direct Line:</strong>{props.info.direct}</Text>
         </Box>
     </Article>
 )
