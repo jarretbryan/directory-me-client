3befb903b4fa680a07a79be5477484ba198a9b37
jarretbryan <jarret.bryan@gmail.com>
2018-11-04 14:16:54 -0500

    cleanup

--- src/Components/EmployeeCard.js
+++ src/Components/EmployeeCard.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const EmployeeCard = (props) => (
+    <Article>
+        <Title>{props.info.name}</Title>
+        <Box>
+            <Text><strong>Department: </strong>{props.info.department.name} </Text>
+            <Text><strong>Title: </strong>{props.info.title}</Text>
+            <Text><strong>Office: </strong>{props.info.office}</Text>
+            <Text><strong>Direct Line: </strong>{props.info.direct}</Text>
+        </Box>
+    </Article>
+)
 
 const Article = styled.article.attrs({
     className: "mw5 center bg-white br3 pa3 pa4-ns mv3"
@@ -17,18 +28,5 @@ const Box = styled.div.attrs({
 const Text = styled.h2.attrs({
     className: "f5 fw4 gray mt0"
 })``
-
-
-const EmployeeCard = (props) => (
-    <Article>
-        <Title>{props.info.name}</Title>
-        <Box>
-            <Text><strong>Department: </strong>{props.info.department.name} </Text>
-            <Text><strong>Title: </strong>{props.info.title}</Text>
-            <Text><strong>Office: </strong>{props.info.office}</Text>
-            <Text><strong>Direct Line: </strong>{props.info.direct}</Text>
-        </Box>
-    </Article>
-)
 
 export default EmployeeCard
